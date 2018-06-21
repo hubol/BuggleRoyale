530e09513a384320b14d179f13c362786cce5665
UltimateWalrus <ultimatewalrus@gmail.com>
2018-06-21 13:01:02 -0700

    Added gem object

---
+++ Assets/Scripts/Animation/SimpleSpin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Spins object around y axis
+public class SimpleSpin : MonoBehaviour
+{
+	public float spinSpeed = 0.1f;
+
+	Quaternion baseRot;
+
+	float yangle=0;
+
+	private void Start()
+	{
+		baseRot = transform.localRotation;	
+	}
+
+	private void Update()
+	{
+		yangle += Time.deltaTime * spinSpeed;
+		transform.localRotation = Quaternion.Euler(0, yangle, 0) * baseRot;
+	}
+
+}
