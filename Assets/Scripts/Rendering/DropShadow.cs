b11a5818ee2d9926484af5b1734ce06f1eaf07a4
UltimateWalrus <ultimatewalrus@gmail.com>
2018-06-21 15:01:51 -0700

    Fixed bug with drop shadows

--- Assets/Scripts/Rendering/DropShadow.cs
+++ Assets/Scripts/Rendering/DropShadow.cs
@@ -9,8 +9,12 @@ using UnityEngine;
 public class DropShadow : MonoBehaviour
 {
 	public Transform targetTransform;  //drops a shadow from this transform
-	public MonoBehaviour disableOnOutOfBounds;    //the component to enable/disable depending on if we went out of bounds
+	public Sprite shadowSprite;    //render the sprite manually so parenting doesn't cause issues
+	public float bias = 0.0f;
+	public IsoRender isoRender;
 	public TerrainGrid terrainGrid;
+	
+
 
 
 	void Update()
@@ -24,25 +28,20 @@ public class DropShadow : MonoBehaviour
 		bool out_of_bounds = x<0 || x >= terrainGrid.xsize || y < 0 || z <0 || z >= terrainGrid.zsize;   //it's fine if y >= terrainGrid.ysize, because the shadow will just go downward
 
 
-		if( out_of_bounds )
-			disableOnOutOfBounds.enabled = false;
-		else
+		if(!out_of_bounds)
 		{
 			//start at x,y,z and move down until we hit a block.  Then, just place ourselves on top of that block
 			for(; y >= 0; y--)
 				if(y<terrainGrid.ysize && terrainGrid.grid[x,y,z] != TerrainGrid.GridCell.NONE)
 				{
 					//found non-empty cell; place ourselves on top and get outta here
-					transform.position = new Vector3(targetTransform.position.x, y+1, targetTransform.position.z);   //use world space
+					isoRender.RenderFreeObject(shadowSprite, new Vector3(targetTransform.position.x, y+1, targetTransform.position.z), bias);   //use world space
 					break;
 				}
-
-			if( y < 0 )   //went out of bounds
-				disableOnOutOfBounds.enabled = false;
-			else
-				disableOnOutOfBounds.enabled = true;  //found a place to put the shadow
 		}
 		
 				
 	}
+
+
 }
